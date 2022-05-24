ecbd9e162401d27b60f504f074badaaf3e701e81
sanjaykrishna1203 <sanjaykrishna1203@gmail.com>
2022-05-24 09:34:48 +0530

    feat(Mailer): Implemented Mailer

---
+++ functions/model/Mailer/tark/generateTemplate.js
@@ -0,0 +1,56 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable max-len */
+ const { getTemplate } = require("./getTemplate");
+
+ /**
+  * Description
+  * @param {any} mailType
+  * @param {any} applicantName
+  * @param {any} applicationId
+  * @return {any}
+  */
+ exports.generateTemplate = function(mailType, applicantName, applicationId) {
+   const message = [];
+   let subjectMessage = "";
+   let templateName = "";
+   let mailSubject;
+   if (mailType == "Registration_Complete") {
+       console.log("registration");
+     templateName = "registration.html";
+   } else if (mailType == "Payment_Complete") {
+     templateName = "payment.html";
+   } else if (mailType == "Selected_Email") {
+    templateName = "selected.html";
+   } else if (mailType == "Rejected_Email") {
+    templateName = "rejected.html";
+   }
+   const promise = getTemplate(templateName).then((data) => {
+     if (templateName == "registration.html") {
+        mailSubject="Registration Successful - GCPA";
+        data = data.replace("$Name$", applicantName);
+        data = data.replace("$applicationLink$", "https://www.gcpawards.com/"); // applicationId
+     } else if (templateName == "payment.html") {
+        data = data.replace("$Name$", applicantName);
+        data = data.replace("$applicationLink$", "https://www.gcpawards.com/"); // applicationId
+        mailSubject="Payment Successful - GCPA";
+     } else if (templateName == "selected.html") {
+        data = data.replace("$Name$", applicantName);
+        mailSubject="Congratulations on the Global Child Prodigy Award";
+        data = data.replace("$applicationLink$", "https://www.gcpawards.com/"); // applicationId
+    } else if (templateName == "rejected.html") {
+        data = data.replace("$Name$", applicantName);
+        mailSubject="Application Status - GCPA";
+    }
+     subjectMessage = mailSubject;
+     message.push(subjectMessage);
+     message.push(data);
+     return message;
+   }).catch((err) => {
+     console.error(err);
+     return;
+   });
+   return Promise.resolve(promise);
+ };
