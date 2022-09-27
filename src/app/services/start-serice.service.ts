8941bd8a3cc794c71baaa76d102bb146fa0f8bc3
sanjaykrishna1203 <sanjaykrishna1203@gmail.com>
2022-09-27 22:23:04 +0530

    fix(gallery):Removed console logs

--- src/app/services/start-serice.service.ts
+++ src/app/services/start-serice.service.ts
@@ -7,18 +7,7 @@ import { GalleryDashboardService } from './gallery/gallery-dashboard.service';
 export class StartSericeService {
 
   constructor(private galleryService: GalleryDashboardService) { }
-  gallery:any;
   StartApplication(){
-    this.galleryService.getphoto(0,20).subscribe({
-      next: (data) => {
-        this.gallery = data;
-      },
-      error: (error) => {
-        console.error(error);
-      },
-      complete: () => {
-        console.info('Getting Photos successful')
-      }
-      });
+    this.galleryService.getphoto(0,20).subscribe((data)=>{});
   }
 }
